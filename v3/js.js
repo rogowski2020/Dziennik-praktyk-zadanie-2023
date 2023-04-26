98cbe70c91e7cd8cf7a2ccd4dc2722336e563815
rogowski2020 <102522249+rogowski2020@users.noreply.github.com>
2023-04-26 21:29:23 +0200

    Add files via upload

---
+++ v3/js.js
@@ -0,0 +1,12 @@
+function funkcja() {
+    const jspole = document.getElementById("pole");
+    //let name = document.getElementById("name").value;
+    const jsname = document.getElementById("name")
+
+
+
+    jspole.innerHTML=jsname
+
+   // document.getElementById("name").value
+    //document.write(id="name");
+  }
